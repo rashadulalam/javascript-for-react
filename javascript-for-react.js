e1dd47a618b907a82bf6371004b582bc34379a70
rashadulalam <rashadul91@gmail.com>
2023-02-02 08:13:34 +0600

    Declaring variable using var, let, const

--- javascript-for-react.js
+++ javascript-for-react.js
@@ -1,4 +1,20 @@
-// (1) var let const
+// (1) var, let, const
 
-// var example
-var firstName
+// var example 
+// var firstName = "Biswajit";
+// console.log(firstName)
+// firstName = "Indrojit"
+// console.log(firstName)
+
+// // let example
+// let lastName = "Adhikary";
+// console.log(lastName);
+// lastName = "Saha";
+// console.log(lastName);
+
+// cosnt example
+// const firstName = "Biswajit";
+// console.log(firstName);
+// firstName = "Alam";
+// console.log(firstName);
+
