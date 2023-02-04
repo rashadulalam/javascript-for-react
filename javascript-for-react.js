78000ee73efc21aed56141fb2950314cbd318cbc
rashadulalam <rashadul91@gmail.com>
2023-02-04 15:53:27 +0600

    JavaScrit Arrow function

--- javascript-for-react.js
+++ javascript-for-react.js
@@ -18,3 +18,26 @@
 // firstName = "Alam";
 // console.log(firstName);
 
+
+// (2) Arro function
+// Example 1
+function showText( txt) {
+    console.log(txt)
+}
+// showText("Hello world!");
+
+// Example 2
+const showTxt = (txt) => {
+    console.log(txt)
+}
+
+// Example 3
+const firstName = () => {
+    console.log("Rashadul");
+}
+firstName();
+
+// Example 4
+const t = new Date();
+const age = birthYear => t.getFullYear() - birthYear;
+// console.log(age(1991));
